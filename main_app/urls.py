d0c18dd493a1fcd182eae86ec9f6e86b76c7e715
Dalton <daltonvaren@gmail.com>
2020-10-02 10:51:13 -0700

    adds cat index hard coded data

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('api/', views.api, name='api')
+    path('api/', views.api, name='api'),
+    path('cats/', views.cats_index, name='cats_index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,21 @@ def about(request):
 
 def api(request):
     return JsonResponse({"status":200})
+
+# Add the Cat class & list and view function below the imports
+class Cat:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+cats = [
+  Cat('Lolo', 'tabby', 'foul little demon', 3),
+  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
+def cats_index(request):
+    context = {'cats': cats}
+    return render(request, 'cats/index.html', context)
