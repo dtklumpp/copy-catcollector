144e2b068185562b2b1c3b5d0a4f0f31dafb1672
micahwierenga <wierenga.micah@gmail.com>
2020-10-02 14:26:02 -0600

    Adds Cat model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+
+# Ways to create data:
+# 1. Through forms in the app
+# 2. Through the Django/Python shell (python3 manage.py shell)
+# 3. Through the psql shell
