ac025196783bf4cf868a0dc274d9d39d6d1949ea
Dalton <daltonvaren@gmail.com>
2020-10-05 09:00:56 -0700

    adds form to index view

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,8 @@
 from django.forms import ModelForm
 from .models import Cat
 
+
 class Cat_Form(ModelForm):
     class Meta:
-      model = Cat
-      fields = ['name','breed','description','age']
+        model = Cat
+        fields = ['name', 'breed', 'description', 'age']
